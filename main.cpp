7374f593e0c3766c65bf6a96ee97282929e47acb
omarabdelaaty06 <omar_abdelaaty@aucegypt.edum>
2026-04-23 10:11:27 +0000

    Fix memory bugs identified using Valgrind Memcheck

--- main.cpp
+++ main.cpp
@@ -1,72 +1,63 @@
 #include <iostream>
+
 using namespace std;
 
 struct Node {
-  int value;
-  Node* next;
+    int value;
+    Node* next;
 };
 
 Node* createList(int n) {
-  Node* head = new Node;
-  head->value = 0;
-  head->next = nullptr;
+    Node* head = new Node;
+    head->value = 0;
+    head->next = nullptr;
 
-  Node* current = head;
-
-  for (int i = 1; i < n; i++) {
-    current->next = new Node;
-    current = current->next;
-    current->value = i;
-    current->next = nullptr;
-  }
-
-  return head;
+    Node* current = head;
+    for (int i = 1; i < n; i++) {
+        current->next = new Node;
+        current = current->next;
+        current->value = i;
+        current->next = nullptr;
+    }
+    return head;
 }
 
 void printList(Node* head) {
-  Node* temp = head;
-
-  while (temp != nullptr) {
-    cout << temp->value << " ";
-    temp = temp->next;
-  }
-  cout << endl;
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
 }
 
 void freeList(Node* head) {
-  Node* temp = head;
-
-  while (temp != nullptr) {
-    Node* next = temp->next;
-    delete temp;
-    temp = next;
-  }
+    Node* temp = head;
+    while (temp != nullptr) {
+        Node* next = temp->next;
+        delete temp;
+        temp = next;
+    }
 }
 
 int main() {
-  Node* list = createList(5);
+    Node* list = createList(5);
+    printList(list);
 
-  printList(list);
+    freeList(list);
+    list = nullptr;
 
-  freeList(list);
+    Node* leakNode = new Node;
+    leakNode->value = 99;
+    leakNode->next = nullptr;
+    cout << leakNode->value << endl;
+    delete leakNode;
 
-  cout << list->value << endl;
+    int* arr = new int[3];
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 999;
+    delete[] arr;
 
-  freeList(list);
-
-  Node* leakNode = new Node;
-  leakNode->value = 99;
-  leakNode->next = nullptr;
-
-  cout << leakNode->value << endl;
-
-  int* arr = new int[3];
-  arr[0] = 1;
-  arr[1] = 2;
-  arr[2] = 3;
-  arr[5] = 999;
-
-  delete[] arr;
-
-  return 0;
+    return 0;
 }
